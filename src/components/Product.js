5c03dda29d41b300fcee13b2ce0311f0b35a1b0a
AKSHAYKP007 <akshaykp2661998@gmail.com>
2022-11-18 14:52:49 +0530

    the main built

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,16 +2,12 @@ import React from 'react'
 import { NumericFormat } from 'react-number-format';
 
 const Product = ({product}) => {
-  const rating = Math.floor(Math.random() * (6 - 1)) + 1;
-  const p='hello my name is akshay im coming from thrissur'
+  
   
   return (
     <div className='text-xs w-1/5 bg-white m-2 p-2 flex flex-col items-center justify-evenly'>
         <p>{product.title}</p>
         <img  className='h-32' src={product.image} alt='' />
-        <div>
-            {rating}
-        </div>
         <NumericFormat value={product.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
         <button className='bg-orange-500 mb-3 p-3 text-white font-w'>Add to Cart</button>
     </div>
